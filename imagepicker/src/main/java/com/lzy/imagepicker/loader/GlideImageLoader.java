5783986e648fb270a15f800a8c2213ef967a3110
liaoziyao <liaojeason@126.com>
2016-06-15 19:01:35 +0800

    升级0.2.6,fix #8,图片路径含有%无法显示的问题

--- imagepicker/src/main/java/com/lzy/imagepicker/loader/GlideImageLoader.java
+++ imagepicker/src/main/java/com/lzy/imagepicker/loader/GlideImageLoader.java
@@ -1,11 +1,14 @@
 package com.lzy.imagepicker.loader;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.lzy.imagepicker.R;
+
+import java.io.File;
 
 /**
  * ================================================
@@ -21,7 +24,7 @@ public class GlideImageLoader implements ImageLoader {
     @Override
     public void displayImage(Activity activity, String path, ImageView imageView, int width, int height) {
         Glide.with(activity)                             //配置上下文
-                .load("file://" + path)                  //设置图片路径
+                .load(Uri.fromFile(new File(path)))      //设置图片路径(fix #8,文件名包含%符号 无法识别和显示)
                 .error(R.mipmap.default_image)           //设置错误图片
                 .placeholder(R.mipmap.default_image)     //设置占位图片
                 .diskCacheStrategy(DiskCacheStrategy.ALL)//缓存全尺寸
