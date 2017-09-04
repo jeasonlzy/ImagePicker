06e15faeffc6ce8276865b213c2592c00f7e9b2f
zchu <zhaocz@pifubao.com>
2017-09-04 14:45:52 +0800

    优化图片选择页UI，在图片上铺上一层阴影，防止白色图片看不到选中框

--- imagepicker/src/main/java/com/lzy/imagepicker/ui/ImagePreviewDelActivity.java
+++ imagepicker/src/main/java/com/lzy/imagepicker/ui/ImagePreviewDelActivity.java
@@ -2,7 +2,6 @@ package com.lzy.imagepicker.ui;
 
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.res.Configuration;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
